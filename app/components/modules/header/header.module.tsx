04349cd9afd04f012ecd386e6c472030880c38fe
Nasir Movlamov <movlamovnasir2000@gmail.com>
2023-04-02 23:54:36 +0400

    feat: chatpgpt module closed

--- app/components/modules/header/header.module.tsx
+++ app/components/modules/header/header.module.tsx
@@ -57,13 +57,13 @@ export const HeaderModule: React.FC = (props: Props) => {
                 </span>
               </Link>
             </li>
-            <li>
+            {/* <li>
               <Link href="/chatgpt" passHref>
                 <span className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0  md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
                   ChatGPT
                 </span>
               </Link>
-            </li>
+            </li> */}
             <li>
               <Link href="/about" passHref>
                 <span className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0  md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
